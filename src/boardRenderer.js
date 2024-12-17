ee044b5ab7f522947962d93ba326f3039bd7c0eb
OmegaGinga <abdielflores980401@hotmail.com>
2024-12-17 09:14:12 -0600

    Created UI for start the game Can assign names to the players o computer The game render all boards when finished

--- src/boardRenderer.js
+++ src/boardRenderer.js
@@ -1,6 +1,4 @@
-const { Player } = require('./player.js');
-
-function renderBoard(player){
+function renderBoard(player) {
     const playerBoard = player.getPlayerBoard();
     const gameBoard = document.querySelector('.game-board');
     const board = document.createElement('div');
@@ -31,20 +29,24 @@ function renderBoard(player){
         
         cell.classList.add(`pos_${node.x}_${node.y}`, 'cell', 'click-pointer', 'drop-zone');
         board.appendChild(cell);
-        
-        cell.addEventListener('click', function handleClick(){
-            const result = player.receiveAttack(node.x, node.y);
-    
-            if (result === 'Hit' || result === 'Sunk') {
-                cell.classList.add('hit');
-            } else if (result === 'Missed') {
-                cell.classList.add('missed');
-                cell.textContent = 'X';
-            }
 
-            cell.removeEventListener('click', handleClick);
-            cell.style.pointerEvents = 'none';
-        });
+        player.playerIsReady();
+        if (player.isPlayerReady()){
+            cell.addEventListener('click', function handleClick() {
+                const result = player.receiveAttack(node.x, node.y);
+    
+                if (result === 'Hit' || result === 'Sunk') {
+                    cell.classList.remove('white');
+                    cell.classList.add('hit');
+                } else if (result === 'Missed') {
+                    cell.classList.add('missed');
+                    cell.textContent = 'X';
+                }
+    
+                cell.removeEventListener('click', handleClick);
+                cell.style.pointerEvents = 'none';
+            });
+        }
 
         cell.addEventListener('dragover', (event) => {
             event.preventDefault();
@@ -55,17 +57,32 @@ function renderBoard(player){
         
             const shipLength = parseInt(event.dataTransfer.getData('text/plain'), 10);
             const direction = event.dataTransfer.getData('direction');
-            playerBoard.placeShip(node.x, node.y, shipLength, direction);
+            const shipId = event.dataTransfer.getData('shipId');
+            
+            if (playerBoard.placeShip(node.x, node.y, shipLength, direction)) {
+                if (direction === 'vertical') {
+                    for (let i = 0; i < shipLength; i++) {
+                        const cellPosition = document.querySelector(`.pos_${node.x+i}_${node.y}`);
+                        cellPosition.classList.add('white');
+                    }
+                } else if (direction === 'horizontal') {
+                    for (let i = 0; i < shipLength; i++) {
+                        const cellPosition = document.querySelector(`.pos_${node.x}_${node.y+i}`);
+                        cellPosition.classList.add('white');
+                    }
+                }
+                const shipDiv = document.getElementById(shipId);
+                shipDiv.remove();
+            }
         });
-        
-
     });
 
-    ships.forEach((shipLength) => {
+    ships.forEach((shipLength, index) => {
         const shipDiv = document.createElement('div');
         shipDiv.classList.add('shipDiv', 'vertical', `length_${shipLength}`);
         shipDiv.setAttribute('draggable', 'true');
-    
+        shipDiv.setAttribute('id', `ship-${index}`);
+
         for (let i = 0; i < shipLength; i++) {
             const ship = document.createElement('div');
             ship.classList.add('ship', 'draggable');
@@ -73,43 +90,39 @@ function renderBoard(player){
         }
 
         shipDiv.addEventListener('click', () => {
-            if (shipDiv.classList.contains('horizontal')){
+            if (shipDiv.classList.contains('horizontal')) {
                 shipDiv.classList.remove('horizontal');
                 shipDiv.classList.add('vertical');
             } else {
                 shipDiv.classList.remove('vertical');
                 shipDiv.classList.add('horizontal');
             }
-        })
+        });
 
         shipDiv.addEventListener('dragstart', (event) => {
             event.dataTransfer.setData('text/plain', shipLength);
-            console.log('dragging ship of length: ', shipLength);
-            if (shipDiv.classList.contains('vertical')){
+            if (shipDiv.classList.contains('vertical')) {
                 const direction = 'vertical';
-                event.dataTransfer.setData('direction',direction);
+                event.dataTransfer.setData('direction', direction);
                 console.log('dragging ship is vertical');
             } else if (shipDiv.classList.contains('horizontal')) {
                 const direction = 'horizontal';
                 event.dataTransfer.setData('direction', direction);
                 console.log('dragging ship is horizontal');
             }
-        })
-    
-        if(shipLength === 1){
+            event.dataTransfer.setData('shipId', shipDiv.id);
+        });
+
+        if (shipLength === 1) {
             shipsContainer1.appendChild(shipDiv);
-        } else if (shipLength === 2){
+        } else if (shipLength === 2) {
             shipsContainer2.appendChild(shipDiv);
-        } else if (shipLength === 3){
+        } else if (shipLength === 3) {
             shipsContainer3.appendChild(shipDiv);
-        } else if (shipLength === 4){
+        } else if (shipLength === 4) {
             shipsContainer4.appendChild(shipDiv);
         }
     });
-
-
-    
-
 };
 
 module.exports = {
